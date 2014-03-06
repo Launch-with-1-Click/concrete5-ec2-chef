e629ba26bf43562a24e2865ef40307dde6d11ccc
miyauchi <miya@wpist.me>
2014-03-07 08:52:23 +0900

    update clear script

--- cleanup.sh
+++ cleanup.sh
@@ -2,18 +2,6 @@
 
 set -ex
 
-echo "Yes? or Ctrl+C"
-read hoge
-
-apt-get clean all
-
-chef-server-ctl stop
-chef-server-ctl cleanse
-rm -rf /root/chef-server-cleanse-*
-sudo install -d /etc/chef-server
-sudo install -o root -g root -m 0640 /vagrant/files/chef-server.rb /etc/chef-server/
-
-
 rm -rf /tmp/*
 rm -rf /vagrant
 rm -f /etc/ssh/ssh_host_*
